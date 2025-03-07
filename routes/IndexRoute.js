28c4fa55c5c6fc5e0a0d9c379c9e4d277c077b31
LenzzzMew <felzjagoxyz@gmail.com>
2025-03-06 19:59:46 -0800

    Update IndexRoute.js

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 // setting the route
 router.get("/", (req, res) => {
-    res.send("Simple reverse proxy by GrowPlus Community");
+    res.send("Simple reverse proxy by FelzStore");
 });
 
 // exporting the router
